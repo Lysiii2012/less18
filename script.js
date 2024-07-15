4383a7983892d6ee4cc0bb7abced41faed6f29a5
Людмила Ивченко <95207405+Lysiii2012@users.noreply.github.com>
2024-07-15 12:38:35 +0300

    res

--- script.js
+++ script.js
@@ -2,20 +2,23 @@ document.addEventListener("DOMContentLoaded", () => {
     const minutesElem = document.querySelector(".min");
     const secondsElem = document.querySelector(".sec");
     
-    let timerSeconds = 5 * 60;  
+    let timerСounter = localStorage.getItem('timerСounter');
+    timerСounter = timerСounter ? parseInt(timerСounter, 10) : 5 * 60;
 
     function updateTimer() {
-        let minutes = Math.floor(timerSeconds / 60);
-        let seconds = timerSeconds % 60;
+        let minutes = Math.floor(timerСounter / 60);
+        let seconds = timerСounter % 60;
 
         minutesElem.textContent = minutes < 10 ? '0' + minutes : minutes;
         secondsElem.textContent = seconds < 10 ? '0' + seconds : seconds;
 
-        if (timerSeconds > 0) {
-            timerSeconds--;
+        if (timerСounter > 0) {
+            timerСounter--; 
+            localStorage.setItem('timerСounter', timerСounter);
         } else {
             clearInterval(timerInterval);
             console.log('Час вийшов');
+            localStorage.removeItem('timerСounter');
         }
     } 
 
